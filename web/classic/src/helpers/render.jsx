08da5c78a1e5fc986e409f42339377f0c4c10049
yang <hzeyu3958@gmail.com>
2026-05-29 11:21:02 +0800

    Wire key pool management UI

--- web/classic/src/helpers/render.jsx
+++ web/classic/src/helpers/render.jsx
@@ -139,6 +139,8 @@ export function getLucideIcon(key, selected = false) {
       return <CreditCard {...commonProps} color={iconColor} />;
     case 'channel':
       return <Layers {...commonProps} color={iconColor} />;
+    case 'key_pools':
+      return <Key {...commonProps} color={iconColor} />;
     case 'redemption':
       return <Gift {...commonProps} color={iconColor} />;
     case 'user':
