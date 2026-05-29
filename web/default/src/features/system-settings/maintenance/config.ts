b96ad89a40fcd42fb7c31930c7d160a38a871c51
yang <hzeyu3958@gmail.com>
2026-05-29 11:24:27 +0800

    Add key pool module setting

--- web/default/src/features/system-settings/maintenance/config.ts
+++ web/default/src/features/system-settings/maintenance/config.ts
@@ -75,6 +75,7 @@ export const SIDEBAR_MODULES_DEFAULT: SidebarModulesAdminConfig = {
   admin: {
     enabled: true,
     channel: true,
+    key_pool: true,
     models: true,
     redemption: true,
     user: true,
